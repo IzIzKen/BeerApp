822a585e300e0f0a2db633766c899b83041a9796
IzIzKen <111420knt@gmail.com>
2022-02-11 21:39:08 +0900

    add today

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\Beer;
 use App\Models\Feeling;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class WebController extends Controller
 {
     public function index()
     {
         $feelings = Feeling::all();
-        $beers = Beer::inRandomOrder()->take(8)->get();
+        $today = Carbon::today();
+        $beers = Beer::inRandomOrder($today->getTimestamp())->take(8)->get();
 
         return view('index', compact('feelings', 'beers'));
     }
